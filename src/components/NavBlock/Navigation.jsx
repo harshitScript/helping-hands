7e3b4e0b2bf0e9bb1bf42afb66933f3c896ae66e
Harshit Bhawsar <harshit.bhawsar@taqtics.co>
2024-02-15 07:46:29 +0000

    initial commit

---
+++ src/components/NavBlock/Navigation.jsx
@@ -0,0 +1,7 @@
+import styles from './Navigation.module.css'
+const Navigation = () => {
+    return <div className={styles.navigation_outer}>
+        <span className={styles.navigation_header}>Helping Hands</span>
+    </div>
+}
+export default Navigation
